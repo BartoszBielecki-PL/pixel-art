8609755cf94efb4c08f52ed4749cc0bd6388f70b
Bartosz Bielecki <Bartosz.m.Bielecki@gmail.com>
2021-03-18 12:17:58 +0100

    complete GridSizeChoose.js

--- src/components/GridSizeChoose.js
+++ src/components/GridSizeChoose.js
@@ -3,15 +3,37 @@ import React, { Component } from "react";
 import "./GridSizeChoose.sass";
 
 class GridSizeChoose extends Component {
-  state = {};
+  state = {
+    width: 1,
+    height: 1,
+  };
+
+  handleWidthChange = (e) => {
+    this.setState({
+      width: e.target.value,
+    });
+  };
+
+  handleHeightChange = (e) => {
+    this.setState({
+      height: e.target.value,
+    });
+  };
+
   render() {
+    const { width, height } = this.state;
+
     return (
       <section className="gridSizeChoose">
         <h2>Grid Size Choose</h2>
         <label htmlFor="pixelWidth">Pixels width</label>
-        <input type="number" />
+        <input type="number" value={width} onChange={this.handleWidthChange} />
         <label htmlFor="pixelHeight">Pixels height</label>
-        <input type="number" />
+        <input
+          type="number"
+          value={height}
+          onChange={this.handleHeightChange}
+        />
       </section>
     );
   }
